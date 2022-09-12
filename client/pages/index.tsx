1416a27b1c35497014cead80abc56e7cb603fcc7
Gurpal Singh <gillgurpal699@gmail.com>
2022-09-12 17:26:39 +0530

    fix: import footer dynamically to reduce bundle size

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,14 @@
-import { Footer, Hero } from "components";
+import { Hero } from "components";
 import { MetaTags } from "components/Meta";
+import dynamic from "next/dynamic";
 import type { NextPage } from "next";
+
+const Footer = dynamic(async () => await (await import("components")).Footer, {
+  ssr: false,
+  loading: () => (
+    <div className="flex flex-col items-center justify-center">Loading...</div>
+  ),
+});
 
 const Home: NextPage = () => {
   return (
