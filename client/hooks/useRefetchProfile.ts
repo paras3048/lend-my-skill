107c7ec54ddd0e01c801597e04f4c14b1f486703
Gurpal Singh <gillgurpal699@gmail.com>
2022-09-12 17:26:10 +0530

    feat: add jwt verification

--- client/hooks/useRefetchProfile.ts
+++ client/hooks/useRefetchProfile.ts
@@ -1,4 +1,6 @@
+import { showNotification } from "@mantine/notifications";
 import axios from "axios";
+import { BACKEND_URL } from "constants/index";
 import { URLGenerator } from "helpers";
 import { createCookie, readCookie } from "helpers/cookies";
 import { useRouter } from "next/router";
@@ -7,11 +9,40 @@ import { useUser } from "./useUser";
 
 export function useRefetchProfile() {
   const { setUser, user } = useUser();
-  const { replace } = useRouter();
+  const { replace, asPath, reload } = useRouter();
   useEffect(() => {
     if (user.id) return;
     const cookie = readCookie("token");
     if (!cookie) return;
+    if (
+      asPath.includes("/dashboard") ||
+      asPath.includes("/orders") ||
+      asPath.includes("/chat")
+    ) {
+      axios
+        .post(`${BACKEND_URL}/jwt`, undefined, {
+          headers: {
+            authorization: cookie,
+          },
+        })
+        .catch((err) => {
+          showNotification({
+            message: err.response?.data?.message || "An Error Occured",
+            color: "red",
+            autoClose: 5000,
+          });
+          if (err.response?.data?.message) {
+            return replace({
+              pathname: "/error/403",
+              query: {
+                append: true,
+                error: err.response.data.message,
+              },
+            });
+          }
+          replace(`/error/403`);
+        });
+    }
     axios
       .get(URLGenerator("FetchProfile"), {
         headers: {
@@ -25,8 +56,7 @@ export function useRefetchProfile() {
           type: "SetUser",
         });
       })
-      .catch((err) => {
-        console.log(err);
+      .catch(() => {
         if (process.env.NODE_ENV !== "development") replace("/error/500");
       });
   }, []);
