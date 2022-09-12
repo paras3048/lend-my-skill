14d3a62843e1930f49ae0eddb09c28bed864ddb2
Gurpal Singh <gillgurpal699@gmail.com>
2022-09-12 19:35:31 +0530

    fix: remove await in import

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,12 +3,17 @@ import { MetaTags } from "components/Meta";
 import dynamic from "next/dynamic";
 import type { NextPage } from "next";
 
-const Footer = dynamic(async () => await (await import("components")).Footer, {
-  ssr: false,
-  loading: () => (
-    <div className="flex flex-col items-center justify-center">Loading...</div>
-  ),
-});
+const Footer = dynamic(
+  () => import("components").then((m) => m.Footer as any),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="flex flex-col items-center justify-center">
+        Loading...
+      </div>
+    ),
+  }
+);
 
 const Home: NextPage = () => {
   return (
