338066f4ac635413a5c3fda4e2f9d0bf7008b42c
Gurpal Singh <gillgurpal699@gmail.com>
2022-09-13 23:36:57 +0530

    refactor: remove scale from route transition

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -37,9 +37,9 @@ function MyApp({ Component, pageProps, router }: AppProps) {
             <AnimatePresence mode="wait">
               <motion.div
                 variants={{
-                  initial: { opacity: 0, scale: 0 },
-                  animate: { opacity: 1, scale: 1 },
-                  exit: { opacity: 0, scale: 0 },
+                  initial: { opacity: 0 },
+                  animate: { opacity: 1 },
+                  exit: { opacity: 0 },
                 }}
                 initial="initial"
                 animate="animate"
