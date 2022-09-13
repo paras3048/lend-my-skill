db4f3e605ad2b894f7d284571f157630e525d281
Gurpal Singh <gillgurpal699@gmail.com>
2022-09-13 23:35:21 +0530

    refacator: show 2 different buttons for auth flow in `Header`

--- client/components/Header/content/index.tsx
+++ client/components/Header/content/index.tsx
@@ -10,21 +10,42 @@ import {
   IconSearch,
 } from "@tabler/icons";
 import { eraseCookie } from "helpers/cookies";
+import Link from "next/link";
 export function NavbarContent() {
-  const { user,setUser } = useUser();
+  const { user, setUser } = useUser();
   const { push } = useRouter();
   const [opened, setOpened] = useState(false);
   const title = opened ? "Close navigation" : "Open navigation";
   if (!user.username)
     return (
-      <Button
-        variant="outline"
-        className={styles.button}
-        onClick={() => push("/auth/signup")}
-        color="dark"
-      >
-        Sign In
-      </Button>
+      <nav className="flex flex-grow">
+        <ul className="flex flex-grow justify-end flex-wrap items-center">
+          <li>
+            <Link href="/auth/login">
+              <a className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">
+                Sign In
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/auth/signup" passHref>
+              <a className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3 cursor-pointer p-4">
+                <span>Sign up</span>
+                <svg
+                  className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
+                  viewBox="0 0 12 12"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                    fillRule="nonzero"
+                  />
+                </svg>
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     );
   return (
     <>
@@ -84,9 +105,8 @@ export function NavbarContent() {
                 icon={<IconTrash size={14} />}
                 onClick={() => {
                   eraseCookie("token");
-                  setUser({type:"Logout",payload:{}})
+                  setUser({ type: "Logout", payload: {} });
                   push("/");
-
                 }}
               >
                 Log Out
