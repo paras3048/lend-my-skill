9898d54ec119b1da3608ccae0b8813af4c41a4a3
Gurpal Singh <gillgurpal699@gmail.com>
2022-09-14 17:36:10 +0530

    refactor: add new feature

--- apps/client/components/Hero/Hero.tsx
+++ apps/client/components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import styles from "./Hero.module.scss";
-import { Button, Divider } from "@mantine/core";
+import { Button } from "@mantine/core";
 import Link from "next/link";
+import { useMediaQuery } from "@mantine/hooks";
 
 const Features = [
   {
@@ -21,6 +22,7 @@ const Features = [
 ];
 
 export function Hero() {
+  const isSmallScreen = useMediaQuery("(max-width:700px)", false);
   return (
     <div className={styles.heroContainer}>
       <h1 data-aos="zoom-y-out" data-aos-delay="300">
@@ -46,6 +48,26 @@ export function Hero() {
       </div>
       <div className={styles.featuresContainer} data-aos="zoom-in">
         <h2 className={styles.feature}>Features</h2>
+        <div
+          className={styles.featureContainer}
+          style={{
+            minWidth: isSmallScreen === false ? "614px" : "unset",
+          }}
+        >
+          <h2 className={styles.featureTitle}>Open Source</h2>
+          <p className={styles.featureDescription}>
+            {" "}
+            Our platform is Open Source. You can read the source code{" "}
+            <a
+              href="https://github.com/phantomknight287/lend-my-skill"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 cursor-pointer"
+            >
+              Here.
+            </a>
+          </p>
+        </div>
         {Features.map((f) => (
           <div key={f.title} className={styles.featureContainer}>
             <h2 className={styles.featureTitle}>{f.title}</h2>
