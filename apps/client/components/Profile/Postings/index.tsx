e8cdbf323383aabf2f1ae160f14341d6f19b57fd
Gurpal Singh <gillgurpal699@gmail.com>
2022-09-17 15:24:12 +0530

    chore: implement pagination in postings

--- apps/client/components/Profile/Postings/index.tsx
+++ apps/client/components/Profile/Postings/index.tsx
@@ -1,4 +1,4 @@
-import { Spoiler, Text } from "@mantine/core";
+import { Button, Group, Spoiler, Text } from "@mantine/core";
 import { GetFilteredHTML } from "helpers";
 import styles from "./posting.module.scss";
 import dashboardStyles from "styles/scss/dashboard.module.scss";
@@ -7,6 +7,10 @@ import { useUser } from "hooks/useUser";
 import dayjs from "dayjs";
 import relative from "dayjs/plugin/relativeTime";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "constants/index";
+import { showNotification } from "@mantine/notifications";
 
 dayjs.extend(relative);
 
@@ -21,8 +25,34 @@ interface PostingsTypes {
 }
 
 export function Postings(props: PostingsTypes) {
-  const { postings } = props;
+  const [postings, setPostings] = useState(props.postings);
   const { query } = useRouter();
+  const [skip, setSkip] = useState(0);
+  const [showButton, setShowButton] = useState(postings.length === 5);
+  const refetchPostings = () => {
+    axios
+      .get(
+        `${BACKEND_URL}/postings/fetch/${query.username}/all?take=5&skip=${
+          skip + 5
+        }`
+      )
+      .then(({ data }) => data)
+      .then((d) => {
+        if (d.length < 5) {
+          setShowButton(false);
+        }
+        setPostings((old) => [...old, ...d]);
+        setSkip((n) => n + 5);
+      })
+      .catch((err) => {
+        return showNotification({
+          message: err.response?.data?.message || "An Error Occured",
+          color: "red",
+          autoClose: 4000,
+        });
+      });
+  };
+
   const {
     user: { username },
   } = useUser();
@@ -32,7 +62,10 @@ export function Postings(props: PostingsTypes) {
         <>
           {postings.map((post) => (
             <div key={post.id} className={styles.container}>
-              <Link passHref href={`/u/${query.username}/${post.slugifiedTitle}`}>
+              <Link
+                passHref
+                href={`/u/${query.username}/${post.slugifiedTitle}`}
+              >
                 <a className={styles.title}>{post.title}</a>
               </Link>
               <Spoiler maxHeight={50} showLabel="Expand" hideLabel="Collapse">
@@ -48,6 +81,17 @@ export function Postings(props: PostingsTypes) {
               </p>
             </div>
           ))}
+          {showButton ? (
+            <Group position="center" mt="xl" mb="xl">
+              <Button
+                color="dark"
+                className="customButton"
+                onClick={refetchPostings}
+              >
+                Load More
+              </Button>
+            </Group>
+          ) : null}
         </>
       ) : (
         <Text>No Posting Available By The User.</Text>
