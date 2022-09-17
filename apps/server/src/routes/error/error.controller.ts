58af41a7a588870b1ae94e7048d6a82c5c30ffb3
Gurpal Singh <gillgurpal699@gmail.com>
2022-09-17 15:30:05 +0530

    chore: create error logging routes(not implemented yet)

---
+++ apps/server/src/routes/error/error.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller,Headers,Post,UnauthorizedException,UseGuards } from '@nestjs/common';
+import axios from 'axios'
+import { WEBHOOK_URL } from 'src/constants';
+import { BaseGuard } from 'src/guards/base.guard';
+import { verifyJWT } from 'src/helpers/jwt';
+import { prisma } from 'src/lib/db';
+@Controller('error')
+@UseGuards(BaseGuard)
+export class ErrorController {
+
+    @Post('/')
+    async reportError(
+        @Body() body:{message:string},
+        @Headers('authorization') auth:string
+    ){
+        const userId = verifyJWT(auth)
+        const user = await prisma.user.findFirst({
+            where:{
+                id:userId
+            },
+            select:{
+                username:true
+            }
+        })
+        if(!user) throw new UnauthorizedException(undefined,"You're not allowed to perform this action.")
+        // axios.post(WEBHOOK_URL,{
+            
+        // })
+    }
+
+}
