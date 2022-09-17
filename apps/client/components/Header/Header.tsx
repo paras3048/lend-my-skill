80d24c15ec87dd87f872259bfb3c15c32258f9c3
Gurpal Singh <gillgurpal699@gmail.com>
2022-09-17 15:23:20 +0530

    refactor: add shadow effect to header

--- apps/client/components/Header/Header.tsx
+++ apps/client/components/Header/Header.tsx
@@ -1,24 +1,24 @@
 import { Header } from "@mantine/core";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./Header.module.scss";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { NavbarContent } from "./content";
 import { useRefetchProfile } from "hooks/useRefetchProfile";
 
 export const Navbar: FC<{}> = () => {
   useRefetchProfile();
+
   return (
     <Header height={70}>
       <div className={styles.headerContainer}>
         <div className={styles.brandName}>
           <Link href="/" passHref>
-            <h2>
+            <h2 className="transition-all duration-300">
               <img
                 src={"/brand/icon-transparent.png"}
                 className={styles.logo}
               />
-              Lend My Skill
+              <span>Lend My Skill</span>
             </h2>
           </Link>
         </div>
